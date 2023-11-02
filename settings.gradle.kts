b02f8f432cd8ad13ee7d1bf4e5b0abf0391aa836
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2023-11-02 15:45:52 +0100

    minor fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
-
-rootProject.name = "final-test"
-
-include("task1")
-
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
-
-    plugins {
-        kotlin("jvm") version "1.7.20"
-        kotlin("plugin.serialization") version "1.7.20"
-    }
-}
+rootProject.name = "pc-test"
